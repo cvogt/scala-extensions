e64a501ae302c3fdad26804fec7532803297e964
Jan Christopher Vogt <oss.nsp@cvogt.org>
2015-02-27 01:47:09 -0500

    Add in/notIn

--- src/main/scala/scalar.scala
+++ src/main/scala/scalar.scala
@@ -14,4 +14,14 @@ object `package`{
     /** chained syntax for if(bool) Some(value) else None */
     def map[T](value: => T) = if(left) Some(value) else None
   }
+  implicit class AnyExtensions[T](val value: T) extends AnyVal{
+    /** Tests whether the given sequence contains this value as an element */
+    def in(seq: Seq[T]) = seq.contains(value)
+    /** Tests whether the given sequence does NOT contains this value as an element */
+    def notIn(seq: Seq[T]) = !seq.contains(value)
+    /** Tests whether the given set contains this value as an element */
+    def in(set: Set[T]) = set.contains(value)
+    /** Tests whether the given set does NOT contains this value as an element */
+    def notIn(set: Set[T]) = !set.contains(value)
+  }
 }
